8c17120aff11f2cb65fd1221bfdc5ce30e29c67a
Tidewater Analytics <redditor.vmsmith@gmail.com>
2015-08-20 15:57:21 -0400

    Update r_features.r

--- r_features.r
+++ r_features.r
@@ -1,5 +1,8 @@
 ### Rick Jones feature
 
-# If you specify a column in a data frame by `df["column.name"]`, it *preserves* the original data structure.
-# If you specify a column by `df[ , "column.name"]` it *simplifies* the data structure to the simplest possible data structure 
-# that can answer the question.
+# If you specify a column in a data frame by:
+    `df["column.name"]`
+# it *preserves* the original data structure.
+# But if you specify a column by:
+    `df[ , "column.name"]` 
+# it *simplifies* the data structure to the simplest possible data structure that can answer the question.
