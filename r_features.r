24ea9d6751ee9daeda19bcb15a03c7a9fdd37493
Tidewater Analytics <redditor.vmsmith@gmail.com>
2015-08-20 15:54:04 -0400

    Initial commit

---
+++ r_features.r
@@ -0,0 +1,1 @@
+`df["column.name"]` *preserves* the original data structure, while `df[ , "column.name"]` *simplifies* the data structure to the simplest possible data structure that can answer the question.
